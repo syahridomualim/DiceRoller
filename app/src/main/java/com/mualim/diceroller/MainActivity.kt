e7f981397d8ddc05222d9df454df81caa404a4ff
Mu'alim Syahrido <syahridomualim@gmail.com>
2021-02-20 15:35:07 +0700

    add modified files

--- app/src/main/java/com/mualim/diceroller/MainActivity.kt
+++ app/src/main/java/com/mualim/diceroller/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.mualim.diceroller
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.mualim.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        status(false)
+
+        mainBinding.button.setOnClickListener {
             Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
 
             // Do a dice roll when the app starts
             rollDice()
+            status(true)
+        }
+
+        mainBinding.btnReset.setOnClickListener {
+            status(false)
+        }
+    }
+
+    private fun status(state: Boolean) {
+        if (state) {
+            mainBinding.imageView.visibility = View.VISIBLE
+            mainBinding.imageView2.visibility = View.VISIBLE
+            mainBinding.btnReset.visibility = View.VISIBLE
+        } else {
+            mainBinding.imageView.visibility = View.GONE
+            mainBinding.imageView2.visibility = View.GONE
+            mainBinding.btnReset.visibility = View.GONE
         }
     }
 
@@ -24,6 +49,7 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
+        val dice2 = Dice(6)
 
         // Determine which drawable resource ID to use based on the dice roll
         val imageDrawable = when (dice.roll()) {
@@ -36,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Determine which drawable resource ID to use based on the dice roll
-        val imageDrawable2 = when (dice.roll()) {
+        val imageDrawable2 = when (dice2.roll()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -45,10 +71,10 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        imageView.contentDescription = imageDrawable.toString()
-        imageView.setImageResource(imageDrawable)
+        mainBinding.imageView.setImageResource(imageDrawable)
 
-        imageView2.contentDescription = imageDrawable2.toString()
-        imageView2.setImageResource(imageDrawable2)
+        mainBinding.imageView2.setImageResource(imageDrawable2)
     }
+
+
 }
